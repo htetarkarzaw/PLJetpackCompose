586a5dcb2b990a4fe7b7458241e97b8895b703f2
htetarkarzaw <arkarzaw412@gmail.com>
2023-02-10 01:50:23 +0630

    testing state

--- app/src/main/java/com/example/pljetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/pljetpackcompose/MainActivity.kt
@@ -5,57 +5,70 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.pljetpackcompose.ui.theme.PLJetpackComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.dota)
-            val description = "This is Dota 2."
-            val title = "Dota2"
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(15.dp)
-            ) {
-                ImageCard(painter = painter, contentDescription = description, title = title)
+            Column(modifier = Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(value = Color.Yellow)
+                }
+                ColorBox(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color = color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
             }
         }
     }
 }
 
 @Composable
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit) {
+    Box(modifier = modifier
+        .background(color = Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f
+                )
+            )
+        })
+}
+
+@Composable
 fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
+    painter: Painter, contentDescription: String, title: String, modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
+        modifier = modifier.fillMaxWidth(), shape = RoundedCornerShape(15.dp), elevation = 5.dp
     ) {
         Box(modifier = Modifier.height(300.dp)) {
             Image(
@@ -70,8 +83,7 @@ fun ImageCard(
                     .background(
                         brush = Brush.verticalGradient(
                             colors = listOf(
-                                Color.Transparent,
-                                Color.Black
+                                Color.Transparent, Color.Black
                             ), startY = 300f
                         )
                     )
