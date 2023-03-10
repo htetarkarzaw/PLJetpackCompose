b9107c71a9c944fe3dbac27bdf7d45129db11276
htetarkarzaw <arkarzaw412@gmail.com>
2023-03-10 15:10:03 +0630

    make circular progress bar.

--- app/src/main/java/com/example/pljetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/pljetpackcompose/MainActivity.kt
@@ -3,65 +3,97 @@ package com.example.pljetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeState by remember {
-                mutableStateOf(200.dp)
-            }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-//                animationSpec = keyframes {
-//                    durationMillis = 5000
-//                    sizeState at 0 with LinearOutSlowInEasing
-//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-//                    sizeState * 2f at 5000 with LinearOutSlowInEasing
-//                }
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioHighBouncy
-                )
-//                animationSpec = tween(durationMillis = 3000,
-//                delayMillis = 300,
-//                easing = LinearOutSlowInEasing)
-            )
-            val infiniteTransition = rememberInfiniteTransition()
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    animation = tween(durationMillis = 2000),
-                    repeatMode = RepeatMode.Reverse
-                )
-            )
             Box(
-                modifier = Modifier
-                    .size(size)
-                    .background(color = color),
+                modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                Button(onClick = {
-                    sizeState += 50.dp
-                }) {
-                    Text(text = "Increase Size")
-                }
+                circularProgressBar(percent = 0.8f, number = 100)
             }
         }
     }
+}
+
+@Composable
+fun circularProgressBar(
+    percent: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(value = false)
+    }
+
+    val currentValue = animateFloatAsState(
+        targetValue = if (animationPlayed) percent else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(radius * 2f)
+        ) {
+            Canvas(modifier = Modifier.size(radius * 2f)) {
+                drawArc(
+                    color = color,
+                    startAngle = -90f,
+                    sweepAngle = 360 * currentValue.value,
+                    useCenter = false,
+                    style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
+                )
+            }
+            Text(
+                text = (currentValue.value * number).toInt().toString(),
+                color = Color.Black,
+                fontSize = fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            animationPlayed = !animationPlayed
+        }) {
+            Text(
+                text = "Click to reload",
+                color = Color.Black,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
+
 }
