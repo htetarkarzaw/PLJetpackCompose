01835da57d34552cbb6625681079b0a8823e341c
htetarkarzaw <arkarzaw412@gmail.com>
2023-03-15 13:13:10 +0630

    music knob with showing percent text

--- app/src/main/java/com/example/pljetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/pljetpackcompose/MainActivity.kt
@@ -1,89 +1,169 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.example.pljetpackcompose
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.pljetpackcompose.ui.theme.PLJetpackComposeTheme
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.dota)
-            val description = "This is Dota 2."
-            val title = "Dota2"
             Box(
+                contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(15.dp)
+                    .fillMaxSize()
+                    .background(Color(0xFF101010)),
             ) {
-                ImageCard(painter = painter, contentDescription = description, title = title)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = Color.Green,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(32.dp)
+                ) {
+                    var volume by remember {
+                        mutableStateOf(0f)
+                    }
+                    val barCount = 20
+                    MusicKnob(modifier = Modifier.size(100.dp)) {
+                        volume = it
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = "${(volume * 100).roundToInt()} %",
+                            color = Color.Green,
+                            fontSize = 20.sp,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        VolumeBar(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(30.dp),
+                            activeBar = (barCount * volume).roundToInt(),
+                            barCount = barCount
+                        )
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
+fun VolumeBar(
+    modifier: Modifier = Modifier,
+    activeBar: Int = 0,
+    barCount: Int = 10
 ) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
+    BoxWithConstraints(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
     ) {
-        Box(modifier = Modifier.height(300.dp)) {
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ), startY = 300f
-                        )
-                    )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 18.sp))
+        val barWidth = remember {
+            constraints.maxWidth / (2f * barCount)
+        }
+        Canvas(modifier = modifier) {
+            for (i in 0 until barCount) {
+                drawRoundRect(
+                    color = if (i in 0..activeBar) Color.Green else Color.DarkGray,
+                    topLeft = Offset(x = i * barWidth * 2f + barWidth / 2f, y = 0f),
+                    size = Size(width = barWidth, height = constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius(0f)
+                )
             }
         }
     }
+}
+
+@Composable
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableStateOf(value = limitingAngle)
+    }
+    var touchX by remember {
+        mutableStateOf(value = 0f)
+    }
+    var touchY by remember {
+        mutableStateOf(value = 0f)
+    }
+    var centerX by remember {
+        mutableStateOf(value = 0f)
+    }
+    var centerY by remember {
+        mutableStateOf(value = 0f)
+    }
+
+    Image(
+        painter = painterResource(id = R.drawable.music_knob),
+        contentDescription = "Music Knob",
+        modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+                            val fixAngle = if (angle in -180f..-limitingAngle) {
+                                360f + angle
+                            } else {
+                                angle
+                            }
+                            rotation = fixAngle
+
+                            val percent = (fixAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percent)
+                            true
+                        } else false
+                    }
+                    else -> false
+                }
+            }
+            .rotate(rotation)
+    )
 }
